5aa380f7f4445b3ae240fd5be4822b7eea41d7be
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2022-06-19 14:04:04 -0400

    add rocks

--- aliska_foundation/rocks.lua
+++ aliska_foundation/rocks.lua
@@ -1,5 +1,6 @@
 local rocks = {'black_sand', 'black_sandstone', 'basalt', 'limestone', 'mud',
-'conglomerate', 'shale', 'schist', 'slate'}
+'conglomerate', 'shale', 'schist', 'slate', 'marble', 'quartzite', 'monzonite',
+'diorite', 'andesite', 'rhyolite', 'granite', 'gabbro', 'gneiss', 'red_mud'}
 
 local function create_def(description, tile, groups)
 	return {
@@ -26,6 +27,9 @@ local rocks_definition = {
 	mud = create_def(
 		'Mud', 'aliska_mud.png', {crumbly=3, soil=1}, 'dirt'
 	),
+	red_mud = create_def(
+		'Red Mud', 'aliska_red_mud.png', {crumbly=3, soil=1}, 'dirt'
+	),
 	conglomerate = create_def(
 		'Conglomerate', 'aliska_conglomerate.png', {crumbly=1, cracky=3}, 'stone'
 	),
@@ -36,7 +40,34 @@ local rocks_definition = {
 		'Schist', 'aliska_schist.png', {cracky=3}, 'stone'
 	),
 	slate = create_def(
-		'Slate', 'aliska_slate.png', {cracky=3}, 'stone'
+		'Slate', 'aliska_slate.png', {cracky=3, stone=1}, 'stone'
+	),
+	marble = create_def(
+		'Marble', 'aliska_marble.png', {cracky=3, stone=1}, 'stone'
+	),
+	quartzite = create_def(
+		'quartzite', 'aliska_quartzite.png', {crumbly=1, cracky=3}, 'stone'
+	),
+	monzonite = create_def(
+		'Monzonite', 'aliska_monzonite.png', {cracky=3, stone=1}, 'stone'
+	),
+	diorite = create_def(
+		'Diorite', 'aliska_diorite.png', {cracky=3, stone=1}, 'stone'
+	),
+	andesite = create_def(
+		'Andesite', 'aliska_andesite.png', {cracky=3, stone=1}, 'stone'
+	),
+	rhyolite = create_def(
+		'Rhyolite', 'aliska_rhyolite.png', {cracky=3, stone=1}, 'stone'
+	),
+	granite = create_def(
+		'Granite', 'aliska_granite.png', {cracky=3, stone=1}, 'stone'
+	),
+	gabbro = create_def(
+		'Gabbro', 'aliska_gabbro.png', {cracky=3, stone=1}, 'stone'
+	),
+	gneiss = create_def(
+		'Gneiss', 'aliska_gneiss.png', {cracky=3, stone=1}, 'stone'
 	),
 }
 
