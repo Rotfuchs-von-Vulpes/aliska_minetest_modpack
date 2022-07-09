3f890fcafeb136686480fffef3e8b258ed754759
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2022-07-09 18:00:01 -0400

    add mapgen and other things

--- aliska_foundation/tree.lua
+++ aliska_foundation/tree.lua
@@ -169,27 +169,6 @@ minetest.register_lbm({
 	end
 })
 
-minetest.register_decoration({
-	name = "default:rubber_tree",
-	deco_type = "schematic",
-	place_on = {"default:dirt_with_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0.0,
-		scale = -0.015,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	biomes = {"deciduous_forest", 'rainforest_swamp', 'rainforest'},
-	y_max = 31000,
-	y_min = 1,
-	schematic = aliska.path.."/schematics/rubber_tree.mts",
-	flags = "place_center_x, place_center_z",
-	rotation = "random",
-})
-
 aliska.register_cooking('aliska_foudation:latex', 'aliska_foudation:rubber')
 aliska.register_craft('aliska_foudation:rubber_wood 4', {1}, {'aliska_foudation:rubber_tree'})
 aliska.register_craft(
